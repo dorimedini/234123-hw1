22ee94ae68c6b7a20ff4342faddb6fc4b2e362c5
dori medini <dorimedini@gmail.com>
2015-04-05 15:20:54 +0300

    הוספת בדיקות פשוטות, רשימת מקרי קצה

--- new_files/syscall_maxproc.c
+++ new_files/syscall_maxproc.c
@@ -7,18 +7,18 @@
 /**
  * System calls
  */
-int sys_set_child_max_proc() {
-
+int sys_set_child_max_proc(int limit) {
+	return limit+1;
 }
 
 
 int sys_get_max_proc() {
-
+	return 2;
 }
 
 
 int sys_get_subproc_count() {
-
+	return 3;
 }
 
 
--- new_files/test.c
+++ new_files/test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "syscall_maxproc.h"
 
 int main() {
-	fork();
+	int a = set_child_max_proc(5);
+	int b = get_max_proc();
+	int c = get_subproc_count();
+	printf("set_max: %d\nget_max: %d\nget_subproc: %d\n",a,b,c);
 	return 0;
 }
