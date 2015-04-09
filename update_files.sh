a5aae0f25022da68051b580cf784b70c1cc5067d
dori medini <dorimedini@gmail.com>
2015-04-09 20:10:08 +0300

    סתם תיקון סקריפט
    לאחר דיבוג מתמשך גילינו ששמעון אפסשיעתיק בלי קשר אם היה שינוי או לא

--- update_files.sh
+++ update_files.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-cp -vu /mnt/hgfs/HW/234123-hw1/new_files/test.c /root/tests/
+cp -v /mnt/hgfs/HW/234123-hw1/new_files/test.c /root/tests/
 cd /root/tests
 dos2unix test.c
-cp -vu /mnt/hgfs/HW/234123-hw1/new_files/c_test_utils.h /root/tests/
+cp -v /mnt/hgfs/HW/234123-hw1/new_files/c_test_utils.h /root/tests/
 cd /root/tests
 dos2unix c_test_utils.h
-cp -vu /mnt/hgfs/HW/234123-hw1/new_files/syscall_maxproc.c /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
+cp -v /mnt/hgfs/HW/234123-hw1/new_files/syscall_maxproc.c /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
 cd /usr/src/linux-2.4.18-14custom/arch/i386/kernel
 dos2unix syscall_maxproc.c
-cp -vu /mnt/hgfs/HW/234123-hw1/new_files/syscall_maxproc.h /usr/include/
+cp -v /mnt/hgfs/HW/234123-hw1/new_files/syscall_maxproc.h /usr/include/
 cd /usr/include
 dos2unix syscall_maxproc.h
-cp -vu /mnt/hgfs/HW/234123-hw1/edited_kernel_files/entry.S /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
+cp -v /mnt/hgfs/HW/234123-hw1/edited_kernel_files/entry.S /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
 cd /usr/src/linux-2.4.18-14custom/arch/i386/kernel
 dos2unix entry.S
-cp -vu /mnt/hgfs/HW/234123-hw1/edited_kernel_files/sched.h /usr/src/linux-2.4.18-14custom/include/linux/
+cp -v /mnt/hgfs/HW/234123-hw1/edited_kernel_files/sched.h /usr/src/linux-2.4.18-14custom/include/linux/
 cd /usr/src/linux-2.4.18-14custom/include/linux
 dos2unix sched.h
-cp -vu /mnt/hgfs/HW/234123-hw1/edited_kernel_files/Makefile /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
+cp -v /mnt/hgfs/HW/234123-hw1/edited_kernel_files/Makefile /usr/src/linux-2.4.18-14custom/arch/i386/kernel/
 cd /usr/src/linux-2.4.18-14custom/arch/i386/kernel
 dos2unix Makefile
-cp -vu /mnt/hgfs/HW/234123-hw1/edited_kernel_files/fork.c /usr/src/linux-2.4.18-14custom/kernel/
+cp -v /mnt/hgfs/HW/234123-hw1/edited_kernel_files/fork.c /usr/src/linux-2.4.18-14custom/kernel/
 cd /usr/src/linux-2.4.18-14custom/kernel
 dos2unix fork.c
-cp -vu /mnt/hgfs/HW/234123-hw1/edited_kernel_files/exit.c /usr/src/linux-2.4.18-14custom/kernel/
+cp -v /mnt/hgfs/HW/234123-hw1/edited_kernel_files/exit.c /usr/src/linux-2.4.18-14custom/kernel/
 cd /usr/src/linux-2.4.18-14custom/kernel
 dos2unix exit.c
